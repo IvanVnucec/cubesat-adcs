40fd2e99fe5af483c39d2bea4bdec0a09cf4f3ce
Ivan Vnucec <ivnucec@example.com>
2021-09-20 09:24:44 +0200

    add reaction wheel code

---
+++ Core/Inc/reaction_wheel.hpp
@@ -0,0 +1,22 @@
+/*
+ * reaction_wheel.hpp
+ *
+ *  Created on: 20. ruj 2021.
+ *      Author: ivnucec
+ */
+
+#ifndef INC_REACTION_WHEEL_HPP_
+#define INC_REACTION_WHEEL_HPP_
+
+
+class ReactionWheel
+{
+private:
+public:
+	ReactionWheel();
+	~ReactionWheel();
+	void setAngularVelocity(int angular_vel);
+};
+
+
+#endif /* INC_REACTION_WHEEL_HPP_ */
---
+++ Core/Src/reaction_wheel.cpp
@@ -0,0 +1,55 @@
+/*
+ * reaction_wheel.cpp
+ *
+ *  Created on: 20. ruj 2021.
+ *      Author: ivnucec
+ */
+
+
+#include "reaction_wheel.hpp"
+
+
+ReactionWheel::ReactionWheel()
+{
+/*
+ * // Initialize pins for motor control
+  pinMode(MOTOR_PIN, OUTPUT);
+  pinMode(MOTOR_DIRECTION_PIN, OUTPUT);
+
+  // Turn off motor
+  analogWrite(MOTOR_PIN, 0u);
+
+  // Set motor direction to default
+  digitalWrite(MOTOR_DIRECTION_PIN, 0u);
+ */
+}
+
+
+ReactionWheel::~ReactionWheel()
+{
+}
+
+
+void ReactionWheel::setAngularVelocity(int angular_vel)
+{
+	/*
+	 * TODO: Change pwm variable to angular_vel
+
+
+	   if (pwm < 0) {
+    digitalWrite(MOTOR_DIRECTION_PIN, MOTOR_DIRECTION_CW);
+  } else {
+    digitalWrite(MOTOR_DIRECTION_PIN, MOTOR_DIRECTION_ACW);
+  }
+
+  if (pwm < 0) {
+    pwm = -pwm;
+  }
+
+  if (pwm > MOTOR_MAX_PWM) {
+    pwm = MOTOR_MAX_PWM;
+  }
+
+  analogWrite(MOTOR_PIN, pwm);
+	 */
+}
