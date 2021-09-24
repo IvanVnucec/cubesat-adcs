9f151498a0780dd3fee7dbcdfe8ee32de71ea489
Ivan Vnucec <ivnucec@example.com>
2021-09-24 13:36:14 +0200

    implement pwm and direction

--- Core/Inc/reaction_wheel.hpp
+++ Core/Inc/reaction_wheel.hpp
@@ -9,11 +9,27 @@
 #define INC_REACTION_WHEEL_HPP_
 
 
+enum ReactionWheelDirection
+{
+	CLOCKWISE,
+	ANTICLOCKWISE
+};
+
+
 class ReactionWheel
 {
 private:
+	int m_ang_vel;	// TODO: define units, write range
+	float m_pwm;	// TODO: define units, write range
+	ReactionWheelDirection m_dir;
+
+	void setDirection(ReactionWheelDirection dir);
+	void startPWM();
+	void stopPWM();
+	void setPWM(float pwm);
+
 public:
-	ReactionWheel();
+	ReactionWheel(int ang_vel=0);
 	~ReactionWheel();
 	void setAngularVelocity(int angular_vel);
 };
--- Core/Src/reaction_wheel.cpp
+++ Core/Src/reaction_wheel.cpp
@@ -1,50 +1,122 @@
 // TODO: Port code in this file
 
 
+#include <limits.h>
+#include <cmath>
 #include "reaction_wheel.hpp"
+#include "tim.h"
+#include "main.h"
 
 
-ReactionWheel::ReactionWheel()
-{
-/*
- * // Initialize pins for motor control
-  pinMode(MOTOR_PIN, OUTPUT);
-  pinMode(MOTOR_DIRECTION_PIN, OUTPUT);
-
-  // Turn off motor
-  analogWrite(MOTOR_PIN, 0u);
-
-  // Set motor direction to default
-  digitalWrite(MOTOR_DIRECTION_PIN, 0u);
+/**
+ * Initialize MCU peripherals.
+ * Set reaction wheel Angular velocity to zero.
+ * @param angular_vel - Default 0 // TODO: add units and ranges here
+ *
  */
+ReactionWheel::ReactionWheel(int ang_vel)
+{
+	startPWM();
+	setAngularVelocity(ang_vel);
 }
 
 
+/**
+ * Set reaction wheel angular velocity to zero and deinitializes MCU peripherals.
+ */
 ReactionWheel::~ReactionWheel()
 {
+	setAngularVelocity(0);
+	stopPWM();
 }
 
 
+/**
+ * Set direction of rotation and the PWM based on angular velocity.
+ * @param angular_vel - // TODO: add units and ranges here
+ */
 void ReactionWheel::setAngularVelocity(int angular_vel)
 {
-	/*
-	 * TODO: Change pwm variable to angular_vel
+
+	// TODO: add assert here
+	m_ang_vel = angular_vel;
+
+	if(m_ang_vel > 0) {
+		// TODO: determine experimentally which direction we should set here
+		setDirection(CLOCKWISE);
+	}
+	else
+	{
+		// TODO: determine experimentally which direction we should set here
+		setDirection(ANTICLOCKWISE);
+	}
+
+	// TODO: convert angular_vel to PWM value (this implementation is for example only!!!)
+	float pwm = std::abs(m_ang_vel) / INT_MAX;
+	setPWM(pwm);
+}
 
 
-	   if (pwm < 0) {
-    digitalWrite(MOTOR_DIRECTION_PIN, MOTOR_DIRECTION_CW);
-  } else {
-    digitalWrite(MOTOR_DIRECTION_PIN, MOTOR_DIRECTION_ACW);
-  }
+/**
+ * Set the GPIO Direction Pin based on direction value.
+ * @param dir - CLOCKWISE or ANTICLOCKWISE
+ * @warning This function depends on the hardware.
+ */
+void ReactionWheel::setDirection(ReactionWheelDirection dir)
+{
+	// TODO: add assert here. See how to assert Enums
+	m_dir = dir;
 
-  if (pwm < 0) {
-    pwm = -pwm;
-  }
+	if(dir == CLOCKWISE)
+	{
+	    // TODO: Figure out digital pin state for clockwise direction
+		HAL_GPIO_WritePin(REACTION_WHEEL_DIR_GPIO_Port, REACTION_WHEEL_DIR_Pin, GPIO_PIN_RESET);
+	}
+	else
+	{
+		// TODO: Figure out digital pin state for anticlockwise direction
+		HAL_GPIO_WritePin(REACTION_WHEEL_DIR_GPIO_Port, REACTION_WHEEL_DIR_Pin, GPIO_PIN_SET);
+	}
+}
 
-  if (pwm > MOTOR_MAX_PWM) {
-    pwm = MOTOR_MAX_PWM;
-  }
 
-  analogWrite(MOTOR_PIN, pwm);
-	 */
+/**
+ * Starts the Timer and PWM.
+ * @warning This function depends on MCU peripherals.
+ */
+void ReactionWheel::startPWM()
+{
+	// start Timer and PWM
+	HAL_TIM_Base_Start(&htim1);
+	HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
+	__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 0);
+}
+
+
+/**
+ * Stops the Timer and PWM.
+ * @warning This function depends on MCU peripherals.
+ */
+void ReactionWheel::stopPWM()
+{
+	// stop PWM and Timer
+	__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 0);
+	HAL_TIM_PWM_Stop(&htim1, TIM_CHANNEL_1);
+	HAL_TIM_Base_Stop(&htim1);
+}
+
+
+/**
+ * Sets the PWM by setting the Timer Compare register.
+ * @param pwm - // TODO: determine range of pwm variable.
+ * @warning This function depends on MCU peripherals.
+ */
+void ReactionWheel::setPWM(float pwm)
+{
+	// TODO: add assert here
+	m_pwm = pwm;
+
+	// TODO: convert pwm to pulse_width
+	uint32_t pulse_width = 0u;
+	__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, pulse_width);
 }
