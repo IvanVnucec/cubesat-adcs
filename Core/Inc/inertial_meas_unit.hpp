69f70c0371f6b6a3596247b33acb574e7fd5d52f
Ivan Vnucec <ivnucec@example.com>
2021-09-24 14:11:14 +0200

    add IMU files

---
+++ Core/Inc/inertial_meas_unit.hpp
@@ -0,0 +1,31 @@
+/*
+ * inertial_meas_unit.hpp
+ *
+ *  Created on: 24. ruj 2021.
+ *      Author: ivnucec
+ */
+
+#ifndef INC_INERTIAL_MEAS_UNIT_HPP_
+#define INC_INERTIAL_MEAS_UNIT_HPP_
+
+
+#include <cstdint>
+
+
+class InertialMeasUnit
+{
+private:
+	const uint8_t m_imu_i2c_address = 0x68;
+
+public:
+	InertialMeasUnit();
+	~InertialMeasUnit();
+	void calibrateGyro();
+	void getDataFromSensorAsync();
+	void getGyroData();
+	void getAccData();
+	void getMagData();
+};
+
+
+#endif /* INC_INERTIAL_MEAS_UNIT_HPP_ */
