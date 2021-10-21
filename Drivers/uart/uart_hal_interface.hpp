e479129c2dc6cca38c6ea69065f6f25a485d288e
Ivan Vnucec <ivnucec@example.com>
2021-10-21 13:59:18 +0200

    create drivers/uart folder

--- Drivers/uart/uart_hal_interface.hpp
+++ Drivers/uart/uart_hal_interface.hpp
@@ -1,15 +1,15 @@
 /*
- * parser_hal_interface.hpp
+ * uart_hal_interface.hpp
  *
  *  Created on: 20. lis 2021.
  *      Author: ivnucec
  */
 
-#ifndef PARSER_HAL_INTERFACE_HPP_
-#define PARSER_HAL_INTERFACE_HPP_
+#ifndef UART_HAL_INTERFACE_HPP_
+#define UART_HAL_INTERFACE_HPP_
 
 void startReceivingFromUart(void);
 void getDataFromUart(std::uint8_t *data, unsigned len);
 void sendDataWithUart(std::uint8_t *data, unsigned len);
 
-#endif /* PARSER_HAL_INTERFACE_HPP_ */
+#endif /* UART_HAL_INTERFACE_HPP_ */
