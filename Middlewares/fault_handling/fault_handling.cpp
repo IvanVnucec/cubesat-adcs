b1b23bd27ab9cc5b941d4f9759b1ee59e3c31df6
Ivan Vnucec <ivnucec@example.com>
2021-12-06 11:41:03 +0100

    transmit fault messages over bluetooth

--- Middlewares/fault_handling/fault_handling.cpp
+++ Middlewares/fault_handling/fault_handling.cpp
@@ -4,6 +4,7 @@
 #include "adcs_tasks.hpp"
 #include "cmsis_os.h"
 #include "main.h"
+#include "parser.hpp"
 #include "portmacro.h"
 #include "task.h"
 
@@ -37,24 +38,54 @@ void assertAndRaiseFault(bool condition, State fault_state)
 
 void faultHandlingThread(void *argument)
 {
+    static bool fault_not_sended = true;
+
     for (;;) {
         State state = getFaultState();
+        std::string fault_str("Fault: ");
 
         switch (state) {
             case State::NO_FAULT: {
-                HAL_GPIO_TogglePin(LD3_GPIO_Port, LD3_Pin);
                 break;
             }
-            case State::I2C_DRIVER_FAULT:
-            case State::MPU9250_FAULT:
-            case State::IMU_FAULT:
-            case State::UART_DRIVER_FAULT:
-            case State::PARSER_FAULT:
-            case State::REACTION_WHEEL_FAULT:
+            case State::I2C_DRIVER_FAULT: {
+                fault_str += "I2C_DRIVER_FAULT";
+                break;
+            }
+            case State::MPU9250_FAULT: {
+                fault_str += "MPU9250_FAULT";
+                break;
+            }
+            case State::IMU_FAULT: {
+                fault_str += "IMU_FAULT";
+                break;
+            }
+            case State::UART_DRIVER_FAULT: {
+                fault_str += "UART_DRIVER_FAULT";
+                break;
+            }
+            case State::PARSER_FAULT: {
+                fault_str += "PARSER_FAULT";
+                break;
+            }
+            case State::REACTION_WHEEL_FAULT: {
+                fault_str += "REACTION_WHEEL_FAULT";
+                break;
+            }
             case State::GENERIC_FAULT:
             default: {
-                HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_SET);
+                fault_str += "GENERIC_FAULT";
                 break;
+            }
+        }
+
+        if (state == State::NO_FAULT) {
+            HAL_GPIO_TogglePin(LD3_GPIO_Port, LD3_Pin);
+        } else {
+            if (fault_not_sended) {    // transmit Fault state only once
+                HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_SET);
+                Parser::sendString(fault_str + '\n');
+                fault_not_sended = false;
             }
         }
 
