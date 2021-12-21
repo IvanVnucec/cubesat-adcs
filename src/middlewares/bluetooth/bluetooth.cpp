873d4d8ad169ba7bc16106f0401243d005249a3b
IvanVnucec <vnucec.ivan@gmail.com>
2021-12-21 11:23:04 +0100

    add bluetooth fun def add semaphores

--- src/middlewares/bluetooth/bluetooth.cpp
+++ src/middlewares/bluetooth/bluetooth.cpp
@@ -1,6 +1,55 @@
+#include "bluetooth.hpp"
+#include "FreeRTOS.h"
+#include "semphr.h"
+
 namespace Bluetooth
 {
 
-#include <bluetooth.hpp>
+static bool initialized = false;
+static SemaphoreHandle_t i2cTxSemaphore = NULL;
+static SemaphoreHandle_t i2cRxSemaphore = NULL;
+
+bool init()
+{
+    i2cTxSemaphore = xSemaphoreCreateBinary();
+    if (i2cTxSemaphore == NULL)
+        goto return_failure;
+
+    i2cRxSemaphore = xSemaphoreCreateBinary();
+    if (i2cRxSemaphore == NULL)
+        goto return_failure;
+
+    initialized = true;
+
+    return true;
+
+return_failure:
+    return false;
+}
+
+bool transmit(const std::uint8_t& data, unsigned len, unsigned timeout_in_ms)
+{
+    if (not initialized)
+        goto return_failure;
+
+    // TODO: add code here
+    return true;
+
+return_failure:
+    return false;
+}
+
+
+bool receive(const std::uint8_t& data, unsigned len, unsigned timeout_in_ms)
+{
+    if (not initialized)
+        goto return_failure;
+
+    // TODO: add code here
+    return true;
+
+return_failure:
+    return false;
+}
 
 } // namespace Bluetooth
--- src/middlewares/bluetooth/bluetooth.hpp
+++ src/middlewares/bluetooth/bluetooth.hpp
@@ -1,6 +1,11 @@
 #pragma once
 
+#include <cstdint>
+
 namespace Bluetooth
 {
+
+bool transmit(const std::uint8_t& data, unsigned len, unsigned timeout_in_ms);
+bool receive(const std::uint8_t& data, unsigned len, unsigned timeout_in_ms);
 
 } // namespace Bluetooth
