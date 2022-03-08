2b837a66156331dfcfaa394c5d5ef0f40e3cb65e
Ivan Vnučec <50591550+IvanVnucec@users.noreply.github.com>
2022-03-08 16:05:02 +0100

    add clang format to installed deps

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -1,5 +1,5 @@
 apt-get update
-apt-get install -y ninja-build cmake gcc-arm-none-eabi gcc cppcheck gdb-multiarch openocd
+apt-get install -y ninja-build cmake gcc-arm-none-eabi gcc cppcheck gdb-multiarch openocd clang-format
 
 # add link arm-none-eabi-gdb -> gdb-multiarch
 ln -s /usr/bin/gdb-multiarch /usr/bin/arm-none-eabi-gdb
