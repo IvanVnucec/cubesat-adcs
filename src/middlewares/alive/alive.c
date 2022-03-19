83ad8a4665a22b76a3ea0828c2e88c5c9be15109
IvanVnucec <vnucec.ivan@gmail.com>
2022-03-19 11:12:22 +0100

    periodically send alive string

--- src/middlewares/alive/alive.c
+++ src/middlewares/alive/alive.c
@@ -15,10 +15,14 @@
 
 #include "FreeRTOS.h"
 #include "bsp/led/bsp_led.h"
+#include "libs/zs040/zs040.h"
 #include "task.h"
 
 /* Private typedef -----------------------------------------------------------*/
 /* Private define ------------------------------------------------------------*/
+#define I_AM_ALIVE_STR ((const uint8_t *)"alive\n")
+#define I_AM_ALIVE_STR_LEN (6u)
+
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
 /* Private function prototypes -----------------------------------------------*/
@@ -26,8 +30,20 @@
 /* Private user code ---------------------------------------------------------*/
 void ALIVE_thread(void *argument)
 {
+    int i = 0;
+
+    ZS040_init();
+
     for (;;) {
         BSP_LED_toggle(BSP_LED_INDEX_0);
+
+        i++;
+        // send alive string every 10th time
+        if (i >= 10) {
+            ZS040_send(I_AM_ALIVE_STR, I_AM_ALIVE_STR_LEN);
+            i = 0;
+        }
+
         vTaskDelay(pdMS_TO_TICKS(100));
     }
 }
